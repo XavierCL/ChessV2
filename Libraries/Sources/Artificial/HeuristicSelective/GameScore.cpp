0d059f91d2ffc7861c7db7999fc38dcf19a03865
Xavier <xavierx-men@hotmail.com>
2018-04-12 14:06:02 -0400

    Removed the now merged probability artificial

--- Libraries/Sources/Artificial/HeuristicSelective/GameScore.cpp
+++
@@ -1,3 +0,0 @@
-#include "../../../Headers/Artificial/HeuristicSelective/GameScore.h"
-
-const double GameScore::AVERAGE_RATIO = 0.00001;
--- Libraries/Sources/Artificial/HeuristicSelective/HeuristicSelectiveGameNode.cpp
+++
@@ -1,4 +0,0 @@
-#include "../../../Headers/Artificial/HeuristicSelective/HeuristicSelectiveGameNode.h"
-
-std::minstd_rand0* HeuristicSelectiveGameNode::GENERATOR = nullptr;
-FixedUnorderedMap<Board, HeuristicSelectiveGameNode*, BoardHash> HeuristicSelectiveGameNode::NODES(1000000);

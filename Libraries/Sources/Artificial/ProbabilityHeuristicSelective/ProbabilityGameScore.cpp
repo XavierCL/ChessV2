31529982331dab10c34c879b6b473cb4a650ccfe
Xavier <xavierx-men@hotmail.com>
2018-11-21 00:37:47 -0500

    Deleted old probability artificial

--- Libraries/Sources/Artificial/ProbabilityHeuristicSelective/ProbabilityGameScore.cpp
+++
@@ -1,3 +0,0 @@
-#include "../../../Headers/Artificial/ProbabilityHeuristicSelective/ProbabilityGameScore.h"
-
-const double ProbabilityGameScore::AVERAGE_RATIO = 0.00001;
--- Libraries/Sources/Artificial/ProbabilityHeuristicSelective/ProbabilityHeuristicSelectiveGameNode.cpp
+++
@@ -1,4 +0,0 @@
-#include "../../../Headers/Artificial/ProbabilityHeuristicSelective/ProbabilityHeuristicSelectiveGameNode.h"
-
-std::minstd_rand0* ProbabilityHeuristicSelectiveGameNode::GENERATOR = nullptr;
-FixedUnorderedMap<Board, ProbabilityHeuristicSelectiveGameNode*, BoardHash> ProbabilityHeuristicSelectiveGameNode::NODES(1000000);
